c1c247a61c6abaa36ca0ca3e178eee181688e347
ottoxgam <stagetech1@gmail.com>
2025-03-29 16:27:00 -0700

    Restore env variables

--- index.ts
+++ index.ts
@@ -247,10 +247,9 @@ if (!process.env.DISCORD_WEBHOOK_URL) {
   process.exit(-1);
 }
 
-// Manually set the URLs
-//const baWebhookUrl = process.env.DISCORD_WEBHOOK_URL;
-//const baMsWebhookUrl = process.env.DISCORD_MS_WEBOOK_URL;
-//const svWebhookUrl = process.env.SV_DISCORD_WEBHOOK_URL;
+const baWebhookUrl = process.env.DISCORD_WEBHOOK_URL;
+const baMsWebhookUrl = process.env.DISCORD_MS_WEBOOK_URL;
+const svWebhookUrl = process.env.SV_DISCORD_WEBHOOK_URL;
 
 const mesh_topic = process.env.MQTT_TOPIC || "msh/US/CA/socalmesh";
 const grouping_duration = parseInt(process.env.GROUPING_DURATION || "10000");
